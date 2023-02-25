15562cdd9af9c7ca69353b37bed32981b767fe29
stormsidali2001 <assoulsidali@gmail.com>
2023-02-25 10:20:20 +0100

    ...

---
+++ microservices_basic_synchrounous_communication/ms-formation/src/main/java/com/example/msformation/proxy/EtudiantProxy.java
@@ -0,0 +1,17 @@
+package com.example.msformation.proxy;
+
+import com.example.msformation.model.Etudiant;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name="ms-scolarity",url="localhost:8083")
+
+public interface EtudiantProxy {
+    @GetMapping("/etudiants/search/findEtudiantsByIdFormation")
+    CollectionModel<Etudiant> getEtudiant(@RequestParam("idf") Long idf , @RequestParam("projection") String projection);
+}

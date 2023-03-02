44c02a4ceb02df225e96fb5ad2656c1867e8dd55
stormsidali2001 <assoulsidali@gmail.com>
2023-03-02 14:16:23 +0100

    debuging the product controller

--- product management and ordering system/product-service/src/main/java/com/stormsidali2001/productservice/controller/ProductController.java
+++ product management and ordering system/product-service/src/main/java/com/stormsidali2001/productservice/controller/ProductController.java
@@ -4,23 +4,23 @@ import com.stormsidali2001.productservice.dto.CreateProductDto;
 import com.stormsidali2001.productservice.dto.ProductResponseDto;
 import com.stormsidali2001.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController()
-@RequestMapping("api/products")
+@RequestMapping("k1")
 public class ProductController {
     @Autowired
     private ProductService productService;
-    @PostMapping()
-    @ResponseStatus(HttpStatus.CREATED)
+
+    @PostMapping
     public void createProduct(@RequestBody() CreateProductDto product){
+
         this.productService.createProduct(product);
     }
-    @GetMapping()
-    @ResponseStatus(HttpStatus.OK)
+
+    @GetMapping
     public List<ProductResponseDto> getAllProduct(){
         return  this.productService.getAllProduct();
     }

ab4b9464fcad6d622daa5ad39cff00e216764684
stormsidali2001 <assoulsidali@gmail.com>
2023-03-10 12:24:17 +0100

    creating the ms library aggregate

--- CQRS-axon/ms-command-library/src/main/java/com/example/mscommandlibrary/aggregate/LibraryAggregate.java
+++ CQRS-axon/ms-command-library/src/main/java/com/example/mscommandlibrary/aggregate/LibraryAggregate.java
@@ -2,6 +2,9 @@ package com.example.mscommandlibrary.aggregate;
 
 import com.example.coreapi.commands.AddBookCommand;
 import com.example.coreapi.commands.LibraryCreationCommand;
+import com.example.coreapi.commands.RemoveBookCommand;
+import com.example.coreapi.events.BookAddedEvent;
+import com.example.coreapi.events.BookRemovedEvent;
 import com.example.coreapi.events.LibraryCreatedEvent;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.CommandHandler;
@@ -18,6 +21,14 @@ import java.util.List;
 @NoArgsConstructor
 public class LibraryAggregate {
 
+
+    @AggregateIdentifier
+    private String libraryId;
+
+    private String name;
+
+    private List<String> isbnBooks;
+
     @CommandHandler
     public LibraryAggregate(LibraryCreationCommand cmd){
         Assert.notNull(cmd.getLibraryId(),"libraryId should be not null");
@@ -26,16 +37,9 @@ public class LibraryAggregate {
                 LibraryCreatedEvent.builder()
                         .libraryId(cmd.getLibraryId())
                         .name(cmd.getName())
-                .build()
+                        .build()
         );
     }
-    @AggregateIdentifier
-    private String libraryId;
-
-    private String name;
-
-    private List<String> isbnBooks;
-
     //restoring aggregate state
     @EventSourcingHandler
     public void on(LibraryCreatedEvent event){
@@ -47,7 +51,45 @@ public class LibraryAggregate {
     @CommandHandler
     public void handles(AddBookCommand cmd) throws  Exception{
         Assert.notNull(cmd.getLibraryId(),"libraryId should be not null");
+        Assert.notNull(cmd.getIsbn(),"isbn should be not null");
         Assert.notNull(cmd.getTitle(),"title should be not null");
 
+        if(this.isbnBooks.contains(cmd.getIsbn())){
+            throw new Exception("Book must be unique");
+        }
+        AggregateLifecycle.apply(
+                BookAddedEvent.builder()
+                        .title(cmd.getTitle())
+                        .libraryId(cmd.getLibraryId())
+                        .editeurId(cmd.getEditeurId())
+                        .isbn(cmd.getIsbn())
+                .build()
+        );
+    }
+    @EventSourcingHandler
+    public void on(BookAddedEvent event){
+        this.isbnBooks.add(event.getIsbn());
+    }
+
+    @CommandHandler
+    public void handler(RemoveBookCommand cmd) throws  Exception{
+        Assert.notNull(cmd.getIsbn(),"isbn can't be null");
+        Assert.notNull(cmd.getLibraryId(),"libraryId should be not null");
+        if(!this.isbnBooks.contains(cmd.getIsbn())){
+            throw new Exception("Book isbn must exist");
+        }
+        //progpagate the event
+        AggregateLifecycle.apply(
+                BookRemovedEvent.builder()
+                        .libraryId(cmd.getLibraryId())
+                        .isbn(cmd.getIsbn())
+                        .build()
+        );
+
+    }
+
+    @EventSourcingHandler
+    public void on( BookRemovedEvent event){
+        this.isbnBooks.remove(event.getIsbn());
     }
 }

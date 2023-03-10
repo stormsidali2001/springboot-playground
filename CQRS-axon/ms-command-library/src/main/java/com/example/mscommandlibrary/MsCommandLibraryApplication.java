76f28414f9a54cff23c445564a17347050771af7
stormsidali2001 <assoulsidali@gmail.com>
2023-03-10 21:30:25 +0100

    commands are created and the events are published to the axon server

--- CQRS-axon/ms-command-library/src/main/java/com/example/mscommandlibrary/MsCommandLibraryApplication.java
+++ CQRS-axon/ms-command-library/src/main/java/com/example/mscommandlibrary/MsCommandLibraryApplication.java
@@ -1,12 +1,12 @@
 package com.example.mscommandlibrary;
 
-import com.example.coreapi.config.AxonConfiguration;
+import com.example.coreapi.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(AxonConfiguration.class)
+@Import({AxonConfig.class})
 public class MsCommandLibraryApplication {
 
 	public static void main(String[] args) {
